eb85afcc61c37cf73a834cd859bd008cc24b938c
Jennifer Fu <jennifer.fu@dominodatalab.com>
2023-05-18 21:45:15 -0700

    SSR changes with React Router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { MemoryRouter, Routes, Route, Link } from "react-router-dom";
+
+const Home = () => (
+  <>
+    <h1>Home</h1>
+    <ul>
+      <li key="1">
+        <Link to="/first-page">First Page</Link>
+      </li>
+      <li key="2">
+        <Link to="/second-page">Second Page</Link>
+      </li>
+    </ul>
+  </>
+);
+
+const FirstPage = () => (
+  <>
+    <h1>First Page</h1>
+    <Link to="/">Go back Home</Link>
+  </>
+);
+
+const SecondPage = () => (
+  <>
+    <h1>Second Page</h1>
+    <Link to="/">Go back Home</Link>
+  </>
+);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MemoryRouter>
+      <Routes>
+        <Route exact path="/" element={<Home/>} />
+        <Route path="/first-page" element={<FirstPage/>} />
+        <Route path="/second-page" element={<SecondPage/>} />
+      </Routes>
+    </MemoryRouter>
   );
 }
 
